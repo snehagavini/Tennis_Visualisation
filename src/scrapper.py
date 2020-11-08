2083f84e535eaa4906f152ca4aeb2f19271967e4
snehagavini <snehagavini1995@gmail.com>
2020-11-08 17:47:39 -0500

    Getting the requried URL

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,28 +1,45 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup 
-
-# add the fucntion later to read the url input from CLI/GUI
-url = 'http://www.tennislive.net/'
-# get the html of the url
-req = urlopen(url)
-#save to a html file
-html = str(req.read())
-with open("./data/index.html","w") as f:
-    f.write(html)
-
-soup = BeautifulSoup(html, 'html.parser')
-
-for link in soup.find_all('a'):
-    if link.get('href') == "http://www.tennislive.net/tennis_livescore.php?t=live":
-        current_url = link.get('href')
-    elif link.get('href') =="http://www.tennislive.net/tennis_livescore.php?t=fin":
-        finished_url = link.get('href')
+from bs4 import BeautifulSoup
 
 def get_current_matches(current_url):
-    pass
+    soup = get_url(current_url)
+    match_anchors = soup.findAll("a",{"title":"H2H stats - match details"})
+    match_urls = [i["href"] for i in match_anchors]
+    # for each url get a dataframe row
+    return match_urls
+   
 
-def get_finished_matches(finished_url)
-    pass
+def get_finished_matches(finished_url):
+    soup = get_url(finished_url)
+    match_anchors = soup.findAll("a",{"title":"H2H stats - match details"})
+    match_urls = [i["href"] for i in match_anchors]
+    return match_urls
 
 def get_players_data(match_url):
     pass
+
+def get_menu_links(url):
+    soup = get_url(url)
+    ul = soup.find("ul", {"id": "topmenu_full"})
+    li_list = ul.findChildren("li" , recursive=False)
+    url_list = []
+    for i in [0,2]:
+        url_list.append(li_list[i].findChildren("a")[0]["href"])
+    return url_list[0], url_list[1]
+
+def get_url(url):
+    req = urlopen(url)
+    html = str(req.read())
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+def main():
+    date = '2020-10-31'
+    url = 'http://www.tennislive.net/'
+    curr_url, fin_url = get_menu_links(url)
+    current_df = get_current_matches(curr_url)
+    finished_df = get_finished_matches(f'{fin_url}&dm={date}')
+    print(finished_df)
+
+if __name__ == "__main__":
+    main()
