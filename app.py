8d42152d05cc2748abd0fb2fc57e1cb04177322a
snehagavini <snehagavini1995@gmail.com>
2020-11-26 10:16:26 -0500

    Load any file using plotly dash

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-import dash
-import dash_table
+import os
 import pandas as pd
+from dash import Dash, callback_context, no_update
+from dash.dependencies import Input, Output
+from dash_table import DataTable
+import dash_core_components as dcc
+import dash_html_components as html
 
-df = pd.read_csv('./data/20201031.csv')
+path = "./data/sample_tennis_data_table.csv"
+df = pd.read_csv(path)
 
-app = dash.Dash(__name__)
-
-app.layout = dash_table.DataTable(
-    id='table',
-    columns=[{"name": i, "id": i} for i in df.columns],
-    data=df.to_dict('records'),
+app = Dash(__name__)
+app.layout = html.Div(
+    [
+        dcc.Location(id = 'url', refresh = False),
+        DataTable(
+            id="table",
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict("records"),
+            export_format="csv",
+        )
+    ]
 )
 
-if __name__ == '__main__':
+@app.callback(Output('table', 'data'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    return pd.read_csv(f'./data{pathname}.csv').to_dict('records')
+
+if __name__ == "__main__":
     app.run_server(debug=True)
